d29e4941ac351816f5e763918d80f817dcc12934
anubhav saini <iamanubhavsaini@gmail.com>
2013-07-16 11:48:41 +0530

    Working game. Hurray.
    Draw logic still primitive, non-existant specifically.
    
    
    Signed-off-by: anubhav saini <iamanubhavsaini@gmail.com>

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 typedef enum game_results GameResults;
 enum game_results{
   GR_Viable =  1,
-    GR_O_Won =   2,
-    GR_X_Won =   4,
-    GR_Draw =    8
+  GR_O_Won =   2,
+  GR_X_Won =   4,
+  GR_Draw =    8
 };
 
-char board[3][3] = {' '};
+char board[3][3];
 
+void init_board(void);
 void draw_grid(void);
 void get_input_O(int *, int *);
 void get_input_X(int *, int *);
@@ -21,13 +22,20 @@ GameResults examine_grid(void);//return 1 if game is still viable: if game is wo
 
 int main( int argc, char *argv[] )
 {
-  int column, row;
+  int col, row;
+  int total_chances = 9;
   GameResults result;
+  init_board();
   draw_grid();
-  while((result = examine_grid())&& result == GR_Viable){
-    get_input_O(&column, &row);
-    get_input_X(&column, &row);
+  while(((result = examine_grid()) == GR_Viable) && total_chances-- > 0){
+    if(total_chances % 2 ){
+      get_input_X(&row, &col);
+    }
+    else{
+      get_input_O(&row, &col);
+    }
   }
+  //since game has stopped, check why?
   switch(result){
   case GR_O_Won :
     printf("\nO won. Congragulations."); break;
@@ -39,9 +47,15 @@ int main( int argc, char *argv[] )
     printf("\n Don't know why this message ran. Report Error : Invalid Game Result Case."); break;
   }
 
+
   return 0;
 }
-
+void init_board(void){
+  int i = 0, j = 0;
+  for(i = 0; i < 3; i++)
+    for(j = 0; j < 3; j++)
+      board[i][j] = ' ';
+}
 void draw_grid(void)
 {
   // Print out the grid
@@ -54,46 +68,46 @@ void draw_grid(void)
 
 }
 
-void get_input_O(int *column_p, int *row_p)
+void get_input_O(int *row, int *col)
 {
   // Get input for player 1
-  printf("Player 1 please enter the row and column,\nwhere you wish to place your mark (O): ");
-  scanf("%d %d", column_p, row_p);
+  printf("Player 1 please enter the row and col,\nwhere you wish to place your mark (O): ");
+  scanf("%d %d", row, col);
 
-  while( check_input((*column_p), (*row_p), 1) < 0 ) { // while movement is not permitted.
-    printf("Please select a valid column, row: ");
-    scanf("%d %d", column_p, row_p);
+  while( check_input((*row), (*col), 1) < 0 ) { // while movement is not permitted.
+    printf("Please select a valid row, col: ");
+    scanf("%d %d", row, col);
   }
 
   draw_grid();
 }
 
-void get_input_X(int *column_p, int *row_p)
+void get_input_X(int *row, int *col)
 {
   // Get input for player 2
-  printf("Player 2 please enter the row and column,\nwhere you wish to place your mark (X): ");
-  scanf("%d %d", column_p, row_p);
+  printf("Player 2 please enter the row and col,\nwhere you wish to place your mark (X): ");
+  scanf("%d %d", row, col);
 
-  while( check_input((*column_p), (*row_p), 2) < 0 ) { // while movement is not permitted.
-    printf("Please select a valid column, row: ");
-    scanf("%d %d", column_p, row_p);
+  while( check_input((*row), (*col), 2) < 0 ) { // while movement is not permitted.
+    printf("Please select a valid row, col: ");
+    scanf("%d %d", row, col);
   }
 
   draw_grid();
 }
 
-int check_input(int column, int row, int playerId)
+int check_input(int row, int col, int playerId)
 {
 
-  if( !((column >= 1 && column <= 3) && ( row >= 1 && row <= 3)) ) {
+  if( !((col >= 1 && col <= 3) && ( row >= 1 && row <= 3)) ) {
     printf("Error: Wrong boundaries!\n");
     return -1; // Wrong boundaries
   }
 
-  column -= 1;
-  row    -= 1;
-  if( board[column][row] != 'X' || board[column][row] != 'O' ) { // Move is allowed
-    board[column][row] = (playerId == 1) ? 'O' : 'X';
+  col -= 1;
+  row -= 1;
+  if(board[row][col] != 'X' && board[row][col] != 'O' ) { // Move is allowed
+    board[row][col] = (playerId == 1) ? 'O' : 'X';
   } else {
     printf("Error: There is already a mark there!\n");
     return -2; // Error. There is already a mark in that position
@@ -109,8 +123,13 @@ GameResults examine_grid(){
     if(board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O'){
       return GR_O_Won;
     }
-
+    if(board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X'){
+      return GR_X_Won;
+    }
     //check cols
+    if(board[0][i] == 'O' && board[1][i] == 'O' && board[2][i] == 'O'){
+      return GR_O_Won;
+    }
     if(board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X'){
       return GR_X_Won;
     }
@@ -126,7 +145,7 @@ GameResults examine_grid(){
   //check if game is still playable:
   for(i = 0 ; i < 3; i++){
     for(j = 0; j<3; j++){
-      if(board[i][j] ==' '){
+      if(board[i][j]==' '){
         return GR_Viable;
       }
     }
