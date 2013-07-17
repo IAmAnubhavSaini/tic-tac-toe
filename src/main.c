a8af7098c7a369c2522e699a01a0c401ae0a0500
IAmAnubhavSaini <iamanubhavsaini@gmail.com>
2013-07-17 17:55:11 +0530

    Generating random ids. v1.2.4
    Signed-off-by: IAmAnubhavSaini <iamanubhavsaini@gmail.com>

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@ typedef struct player_info Player;
 struct player_info{
   char *Name;
   char Symbol;
+  char *Id;
 };
 
 /* GLOBALS */
@@ -37,6 +38,7 @@ int examine_input(int, int, int);
 GameResults examine_board(void);
 void publish_results(GameResults result);
 void clear_input_stream(FILE *stream);
+char *generate_random_id(int size);
 
 /*DECLARATIONS*/
 int main( int argc, char *argv[] )
@@ -77,19 +79,30 @@ void setup_game(void){
   players[0]->Name = (char*) malloc(sizeof(char)*PLAYER_NAME_SIZE);
   players[1] = (Player*)malloc(sizeof(Player));
   players[1]->Name = (char*) malloc(sizeof(char)*PLAYER_NAME_SIZE);
-  printf("\nPlayer 1, enter your name:> ");
+  
+  //BUG AVERSION
+  //note: different sizes absolutely gurantees the different ids
+  //generate and assign random ids to the players.
+  players[0]->Id = generate_random_id(7); 
+  players[1]->Id = generate_random_id(8);
+
+  //get their names and symbol
+  //get player 1 info
+  printf("\nPlayer 1 (id: %s), enter your name:> ", players[0]->Id);
   scanf("%s", players[0]->Name);
   clear_input_stream(stdin);
   printf("\nPlayer 1, enter your symbol:> ");
   scanf("%c", &players[0]->Symbol);
   clear_input_stream(stdin);
-  printf("\nPlayer 2, enter your name:> ");
+  //get player 2 info
+  printf("\nPlayer 2 (id: %s), enter your name:> ", players[1]->Id);
   scanf("%s", players[1]->Name);
   clear_input_stream(stdin);
   printf("\nPlayer 2, enter your symbol:> ");
   scanf("%c", &players[1]->Symbol);
   clear_input_stream(stdin);
 
+  //BUG AVERSION. issue 1.
   //check if both player have same symbol, make second player choose different one
   while(players[0]->Symbol == players[1]->Symbol){
     printf("\nBoth players have same symbol. Player 2 choose another:> ");
@@ -142,12 +155,12 @@ void get_player2_input(int *row, int *col)
 int examine_input(int row, int col, int playerId)
 {
   /* return values: 
-   * failure:-
-   * -1 : wrong boundaries
-   * -2 : position already filled
-   *
-   * success: 1
-   */
+  * failure:-
+  * -1 : wrong boundaries
+  * -2 : position already filled
+  *
+  * success: 1
+  */
   if( !((col >= 1 && col <= 3) && ( row >= 1 && row <= 3)) ) {
     printf("Error: Wrong boundaries!\n");
     return -1; // error: wrong boundaries
@@ -203,17 +216,47 @@ GameResults examine_board(){
 }
 void publish_results(GameResults result){
   switch(result){
-    case GR_P1_Won :
-      printf("\nPlayer 1 won. Congragulations %s.", players[0]->Name); break;
-    case GR_P2_Won:
-      printf("\nPlayer 2 won. Congragulations %s.", players[1]->Name); break;
-    case GR_Draw:
-      printf("\nGame ended at draw. Thanks for playing %s and %s.", players[0]->Name, players[1]->Name); break;
-    default :
-      printf("\n Don't know why this message ran. Report Error : Invalid Game Result Case."); break;
-    }
+  case GR_P1_Won :
+    printf("\nPlayer 1 won. Congragulations %s.", players[0]->Name); break;
+  case GR_P2_Won:
+    printf("\nPlayer 2 won. Congragulations %s.", players[1]->Name); break;
+  case GR_Draw:
+    printf("\nGame ended at draw. Thanks for playing %s and %s.", players[0]->Name, players[1]->Name); break;
+  default :
+    printf("\n Don't know why this message ran. Report Error : Invalid Game Result Case."); break;
+  }
 }
 void clear_input_stream(FILE *stream){
   int input;
   while(( input = fgetc(stream)) != EOF && input != '\n');
+}
+char *generate_random_id(int size){
+/* Read logic at
+ * http://stackoverflow.com/questions/2999075/generate-a-random-number-within-range/2999130#2999130
+ * OR
+ * we could have used:
+ * #include <time.h>
+ * #include <stdlib.h>
+ * srand(time(NULL));
+ * int r = rand();
+ * FROM
+ * http://stackoverflow.com/questions/822323/how-to-generate-a-random-number-in-c
+ *
+ */
+  char *id = (char*)malloc(sizeof(char)*(size+1));
+  char *valid_chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+  int i=0;
+  int limit = 63; //validchars count is 64, max index in 63 
+  int divisor = RAND_MAX/(limit+1);
+  int index = 0;
+  
+  for(i = 0; i< size; i++){
+    do{
+      index = rand()/divisor;
+    }while(index>limit);
+
+    id[i] = valid_chars[index];
+  }
+  id[i] = '\0';
+  return id;
 }
